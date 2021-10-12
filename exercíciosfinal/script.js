d271ae6de27a2df77a79484e244c0a8c20afa504
JOSEADERVALDO <ADERVALDO@HOTMAIL.COM>
2021-10-12 17:22:50 -0300

    nnn

--- exercíciosfinal/script.js
+++ exercíciosfinal/script.js
@@ -3,12 +3,25 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 function isNumero(n){
-    if(Number(n) >= 1 && Number(N) <= 100){
+    if(Number(n) >= 1 && Number(n) <= 100){
         return true
     } else {
         return false
     }
 } 
 function inLista(n, 1){
-    if ( 1.)
+    if ( 1.indexOf(Number(n) |= -1){
+        return true
+    } else {
+        return false
+    }
+}
+function adicionar(){
+    if(isNumero(num.value) && |inLista(num.value, valores)){
+        window.alert('tudo ok')
+
+        
+    } else {
+        window.alert('valor inválido ou já encontrado na lista')
+    }
 }

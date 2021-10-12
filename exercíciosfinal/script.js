e208e1ca11aa8802119d01731933c0d9362ae680
JOSEADERVALDO <ADERVALDO@HOTMAIL.COM>
2021-10-12 17:48:53 -0300

    N

--- exercíciosfinal/script.js
+++ exercíciosfinal/script.js
@@ -9,19 +9,38 @@ function isNumero(n){
         return false
     }
 } 
+/*
 function inLista(n, 1){
-    if ( 1.indexOf(Number(n) |= -1){
+    if ( l.indexOf(Number(n) = -1){
         return true
     } else {
         return false
     }
 }
 function adicionar(){
-    if(isNumero(num.value) && |inLista(num.value, valores)){
-        window.alert('tudo ok')
+    if(isNumero(num.value) && inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = valor ${num.value} adicionado.
+        lista.appendChild(item)
 
         
     } else {
         window.alert('valor inválido ou já encontrado na lista')
     }
+    num.value = ''
+num.focus()
 }
+function finalizar(){
+    if(valores.lenght == 0){
+        window.alert ('adicione valores antes de finalizar!'){
+
+        } else {
+            ler tot = valores.length
+            res.innerHTML =''
+            res.innerHTML += `<p> Ao todo, temos ${tot} nÚMEROS CADASTRADOS.</P>`
+        }
+    }
+}
+
+*/
